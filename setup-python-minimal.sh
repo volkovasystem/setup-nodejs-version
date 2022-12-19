c61891c7ac2a7b6a8ff4fd869b4fdd46cfc3f195
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2022-12-19 20:01:56 +0800

    Improved python setup.

--- setup-python-minimal.sh
+++ setup-python-minimal.sh
@@ -3,7 +3,7 @@
 #;	@section: setup python minimal:
 
 #;	@note: install python minimal;
-[[ ! -x /usr/bin/python ]] &&\
+[[ ! -x /usr/bin/python2 || ! -x /usr/bin/python3 ]] &&\
 sudo apt-get install -y python*-minimal;
 
 #;	@section: setup python minimal;
